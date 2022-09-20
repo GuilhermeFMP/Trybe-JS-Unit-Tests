59861f69e6a4450ff80a44018eba673da0d1b4dc
Guilherme Pinho <guilhermemoraispinho@gmail.com>
2022-09-20 18:43:41 -0300

    Requisito 5 feito

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  let student = { name: '' };
+  student.name = name;
+  student.feedback = () => 'Eita pessoa boa!';
+  return student;
+};
+
+console.log(createStudent('Guilherme'));
 
 module.exports = createStudent;
